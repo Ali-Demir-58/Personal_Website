fb30d5df5cdd6a5096f20f18b696363c8099ebe6
Ali-Demir-58 <102052333+Ali-Demir-58@users.noreply.github.com>
2025-05-26 21:47:43 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+const blogPosts = [
+  {
+    title: "İlk Blog Yazım",
+    image: "img/blog1.png",
+    excerpt: "Bugün bloguma başlıyorum! Neler paylaşacağım hakkında bazı düşünceler...",
+    link: "Blog/blog1.html"
+  },
+  {
+    title: "Yapay Zeka Hakkında Ne Düşünüyorum?",
+    image: "img/blog2.png",
+    excerpt: "Son zamanlarda AI hayatımızda nasıl yer ediniyor, benim gözümden...",
+    link: "Blog/blog2.html"
+  },
+  {
+    title: "Hayata Dair Notlarım",
+    image: "img/blog3.png",
+    excerpt: "Bugün bir yolda yürürken aklıma gelen birkaç kareyi paylaşıyorum...",
+    link: "Blog/blog3.html"
+  }
+];
